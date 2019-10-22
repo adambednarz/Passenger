8f6b748101020484664b12191dace36dce883678
adambednarz <45689955+adambednarz@users.noreply.github.com>
2019-10-22 13:01:55 +0200

    added Test Project

---
+++ Passenger.Test/Services/UserServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using System.Threading.Tasks;
+using FluentAssertions.AspNetCore.Mvc;
+using Passenger.Infrastructure.Services;
+using Moq;
+using Passenger.Core.Repositories;
+using AutoMapper;
+using Passenger.Core.Domain;
+
+namespace Passenger.Test.Services
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public async Task Test()
+        {
+            //Arrange
+            var userRapositoryMock = new Mock<IUserRepository>();
+            var userService =  new UserService(userRapositoryMock.Object);
+            
+            // Act 
+            await userService.RegisterAsync("test@email.com", "Test", "passwordtest");
+
+            //Assert
+            userRapositoryMock.Verify(x => x.AddAsync(It.IsAny<User>()), Times.Once);
+        }
+    }
+}
